19f74b33daaf6389ba9d816b556a26a05595e647
Duncan Coutts <duncan@haskell.org>
2009-04-18 10:29:27 -0700

    First go at updating the install.sh
    darcs-hash:20090418172927-adfee-77dfb7a72aacf5839df525e607fd1da6b2624361.gz

--- src/generic-tarball/install.sh
+++ src/generic-tarball/install.sh
@@ -1,147 +1,24 @@
 #!/bin/sh
 
-# A script to bootstrap cabal-install.
-
-# It works by downloading and installing the Cabal, zlib and
-# HTTP packages. It then installs cabal-install itself.
-# It expects to be run inside the cabal-install directory.
-
-# install settings, you can override these by setting environment vars
-PREFIX=${PREFIX:-${HOME}/.cabal}
-#VERBOSE
-#EXTRA_CONFIGURE_OPTS
-
-# programs, you can override these by setting environment vars
-GHC=${GHC:-ghc}
-GHC_PKG=${GHC_PKG:-ghc-pkg}
-WGET=${WGET:-wget}
-CURL=${CURL:-curl}
-TAR=${TAR:-tar}
-GUNZIP=${GUNZIP:-gunzip}
-
-
-# Versions of the packages to install.
-# The version regex says what existing installed versions are ok.
-CABAL_VER="1.6.0.2"; CABAL_VER_REGEXP="1\.6\."   # == 1.6.*
-HTTP_VER="4000.0.4"; HTTP_VER_REGEXP="4000\.0\.[3456789]"
-                                                 # >= 4000.0.3 && < 4000.0.10
-ZLIB_VER="0.5.0.0";  ZLIB_VER_REGEXP="0\.[45]\." # >= 0.4  && < 0.6
-
-HACKAGE_URL="http://hackage.haskell.org/packages/archive"
-
-die () {
-  echo
-  echo "Error during cabal-install bootstrap:"
-  echo $1 >&2
-  exit 2
-}
-
-# Check we're in the right directory:
-grep "cabal-install" ./cabal-install.cabal > /dev/null 2>&1 \
-  || die "The bootstrap.sh script must be run in the cabal-install directory"
-
-${GHC} --numeric-version > /dev/null \
-  || die "${GHC} not found (or could not be run). If ghc is installed make sure it is on your PATH or set the GHC and GHC_PKG vars."
-${GHC_PKG} --version     > /dev/null \
-  || die "${GHC_PKG} not found."
-GHC_VER=`${GHC} --numeric-version`
-GHC_PKG_VER=`${GHC_PKG} --version | cut -d' ' -f 5`
-[ ${GHC_VER} = ${GHC_PKG_VER} ] \
-  || die "Version mismatch between ${GHC} and ${GHC_PKG} If you set the GHC variable then set GHC_PKG too"
-
-# Cache the list of packages:
-echo "Checking installed packages for ghc-${GHC_VER}..."
-${GHC_PKG} list > ghc-pkg.list \
-  || die "running '${GHC_PKG} list' failed"
-
-# Will we need to install this package, or is a suitable version installed?
-need_pkg () {
-  PKG=$1
-  VER_MATCH=$2
-  ! grep " ${PKG}-${VER_MATCH}" ghc-pkg.list > /dev/null 2>&1
-}
-
-info_pkg () {
-  PKG=$1
-  VER=$2
-  VER_MATCH=$3
-
-  if need_pkg ${PKG} ${VER_MATCH}
-  then
-    echo "${PKG}-${VER} will be downloaded and installed."
-  else
-    echo "${PKG} is already installed and the version is ok."
-  fi
-}
-
-dep_pkg () {
-  PKG=$1
-  VER_MATCH=$2
-  if need_pkg ${PKG} ${VER_MATCH}
-  then
-    echo
-    echo "The Haskell package '${PKG}' is required but it is not installed."
-    echo "If you are using a ghc package provided by your operating system"
-    echo "then install the corresponding packages for 'parsec' and 'network'."
-    echo "If you built ghc from source with only the core libraries then you"
-    echo "should install these extra packages. You can get them from hackage."
-    die "The Haskell package '${PKG}' is required but it is not installed."
-  else
-    echo "${PKG} is already installed and the version is ok."
-  fi
-}
-
-build_pkg () {
+install_pkg () {
   PKG=$1
 
-  [ -x Setup ] && ./Setup clean
-  [ -f Setup ] && rm Setup
+  [ -x Setup ] || die "The ${PKG}/Setup script does not exist or cannot be run"
 
-  ${GHC} --make Setup -o Setup \
-    || die "Compiling the Setup script failed"
-  [ -x Setup ] || die "The Setup script does not exist or cannot be run"
+  ./Setup copy ${VERBOSE} \
+    || die "Copying the ${PKG} component failed"
 
-  ./Setup configure --user "--prefix=${PREFIX}" \
-    --with-compiler=${GHC} --with-hc-pkg=${GHC_PKG} \
-    ${EXTRA_CONFIGURE_OPTS} ${VERBOSE} \
-    || die "Configuring the ${PKG} package failed"
-
-  ./Setup build ${VERBOSE} \
-    || die "Building the ${PKG} package failed"
-
-  ./Setup register --inplace ${VERBOSE} \
+  ./Setup register ${VERBOSE} --gen-pkg-config= \
     || die "Registering the ${PKG} package failed"
 }
 
-do_pkg () {
-  PKG=$1
-  VER=$2
-  VER_MATCH=$3
-
-  if need_pkg ${PKG} ${VER_MATCH}
-  then
-    echo
-    echo "Building ${PKG}-${VER}..."
-    cd "${PKG}-${VER}"
-    install_pkg ${PKG} ${VER}
-    cd ..
-  fi
-}
-
 # Actually do something!
-
-dep_pkg "parsec" "2\."
-dep_pkg "network" "[12]\."
-
-info_pkg "Cabal" ${CABAL_VER} ${CABAL_VER_REGEXP}
-info_pkg "HTTP"  ${HTTP_VER}  ${HTTP_VER_REGEXP}
-info_pkg "zlib"  ${ZLIB_VER}  ${ZLIB_VER_REGEXP}
-
-do_pkg "Cabal" ${CABAL_VER} ${CABAL_VER_REGEXP}
-do_pkg "HTTP"  ${HTTP_VER}  ${HTTP_VER_REGEXP}
-do_pkg "zlib"  ${ZLIB_VER}  ${ZLIB_VER_REGEXP}
-
-install_pkg "cabal-install"
+for pkg in $(cat platform.packages); do
+  cd "${pkg}"
+  echo "Installing ${pkg}..."
+  install_pkg ${pkg}
+  cd ..
+done
 
 echo
 echo "==========================================="
