902429c5ba210d396f5f7e5fe624f9ea0ef5747a
Duncan Coutts <duncan@haskell.org>
2009-04-19 05:25:02 -0700

    Add prepare.sh file to make the tarball
    darcs-hash:20090419122502-adfee-03ce2aef67f60b5843c4fd332d5d6bd0132b32d3.gz

---
+++ src/generic/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. tarball/scripts/versions
+
+IMAGE_DIR="haskell-platform-${PLATFORM_VERSION}"
+
+echo "Preparing a tarball for ${IMAGE_DIR}"
+
+rm -rf "${IMAGE_DIR}/"
+mkdir "${IMAGE_DIR}/"
+mkdir "${IMAGE_DIR}/packages"
+mkdir "${IMAGE_DIR}/scripts"
+
+runghc Build.hs ../../haskell-platform.cabal "${IMAGE_DIR}/packages"
+
+cp tarball/packages/core.packages     "${IMAGE_DIR}/packages/"
+cp tarball/scripts/*.sh               "${IMAGE_DIR}/scripts/"
+cp tarball/configure tarball/Makefile "${IMAGE_DIR}/"
+
+tar -c "${IMAGE_DIR}" -zf "${IMAGE_DIR}.tar.gz"
+echo "Created ${IMAGE_DIR}.tar.gz"
