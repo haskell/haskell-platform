66e4419cba8330e3be8bf3d52cfa135e600091b0
Duncan Coutts <duncan@haskell.org>
2009-04-19 08:22:50 -0700

    Fix the install.sh script
    darcs-hash:20090419152250-adfee-2326c8e84125cef20620b920327739763b8a00d1.gz

--- src/generic/tarball/scripts/install.sh
+++ src/generic/tarball/scripts/install.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 
+die () {
+  echo
+  echo "Error:"
+  echo $1 >&2
+  exit 2
+}
+
+[ -e "config.status" ] \
+  || die "Please run ./configure first"
+
+. ./config.status
+
 install_pkg () {
   PKG=$1
 
   [ -x Setup ] || die "The ${PKG}/Setup script does not exist or cannot be run"
 
   ./Setup copy ${VERBOSE} \
-    || die "Copying the ${PKG} component failed"
+    || die "Installing the ${PKG} component failed"
 
   ./Setup register ${VERBOSE} --gen-pkg-config="${PKG}.conf" \
     || die "Generating the registration information for the package ${PKG} failed"
 
-  ${GHC_PKG} update --global "${PKG}.conf" \
-    || die "Registering the package ${PKG} failed"
+  if [ -f ${PKG}.conf ]; then
+    ${GHC_PKG} update --global "${PKG}.conf" \
+      || die "Registering the package ${PKG} failed"
+  fi
 }
 
 # Actually do something!
 cd packages
 for pkg in $(cat platform.packages); do
-  cd "${pkg}"
+  cd "${pkg}" || die "The directory for the component ${PKG} is missing"
   echo "Installing ${pkg}..."
   install_pkg ${pkg}
   cd ..
