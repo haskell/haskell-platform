56276b501a1131d42859160da55c70736f8fecdd
Duncan Coutts <duncan@haskell.org>
2009-04-19 07:46:58 -0700

    Fix prepare script to set exe bits properly
    darcs-hash:20090419144658-adfee-8ee0320ece4447f934b728286397194f4eed8437.gz

--- src/generic/prepare.sh
+++ src/generic/prepare.sh
@@ -4,6 +4,15 @@
 
 IMAGE_DIR="haskell-platform-${PLATFORM_VERSION}"
 
+die () {
+  echo
+  echo "Error:"
+  echo $1 >&2
+  exit 2
+}
+
+which cabal 2> /dev/null || die "The prepare script needs the cabal program"
+
 echo "Preparing a tarball for ${IMAGE_DIR}"
 
 rm -rf "${IMAGE_DIR}/"
@@ -13,9 +22,10 @@ mkdir "${IMAGE_DIR}/scripts"
 
 runghc Build.hs ../../haskell-platform.cabal "${IMAGE_DIR}/packages"
 
-cp tarball/packages/core.packages        "${IMAGE_DIR}/packages/"
-cp -p tarball/scripts/*.sh               "${IMAGE_DIR}/scripts/"
-cp -p tarball/configure tarball/Makefile "${IMAGE_DIR}/"
+cp tarball/packages/core.packages     "${IMAGE_DIR}/packages/"
+cp tarball/scripts/*.sh               "${IMAGE_DIR}/scripts/"
+cp tarball/configure tarball/Makefile "${IMAGE_DIR}/"
+chmod +x "${IMAGE_DIR}/scripts/"*.sh "${IMAGE_DIR}/configure"
 
 tar -c "${IMAGE_DIR}" -zf "${IMAGE_DIR}.tar.gz"
 echo "Created ${IMAGE_DIR}.tar.gz"
