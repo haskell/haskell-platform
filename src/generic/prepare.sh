09d225cbf38d9a5d8ce82dda6889805f4415a914
Duncan Coutts <duncan@haskell.org>
2009-04-19 05:28:04 -0700

    Preserve permissions when makeing tarball image
    darcs-hash:20090419122804-adfee-4fcf12151fd1c142fa3db549db8f1bc49abff41a.gz

--- src/generic/prepare.sh
+++ src/generic/prepare.sh
@@ -13,9 +13,9 @@ mkdir "${IMAGE_DIR}/scripts"
 
 runghc Build.hs ../../haskell-platform.cabal "${IMAGE_DIR}/packages"
 
-cp tarball/packages/core.packages     "${IMAGE_DIR}/packages/"
-cp tarball/scripts/*.sh               "${IMAGE_DIR}/scripts/"
-cp tarball/configure tarball/Makefile "${IMAGE_DIR}/"
+cp tarball/packages/core.packages        "${IMAGE_DIR}/packages/"
+cp -p tarball/scripts/*.sh               "${IMAGE_DIR}/scripts/"
+cp -p tarball/configure tarball/Makefile "${IMAGE_DIR}/"
 
 tar -c "${IMAGE_DIR}" -zf "${IMAGE_DIR}.tar.gz"
 echo "Created ${IMAGE_DIR}.tar.gz"
