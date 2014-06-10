2aad7a509dd03c36459990fa2552f2b37acc0d80
Yitzchak Gale <gale@sefer.org>
2014-06-10 23:25:40 +0300

    No need to get newer cabal for travis.

--- travis-setup.sh
+++ travis-setup.sh
@@ -4,16 +4,6 @@ GHC_VER=7.8.2
 GHC_TARBALL=ghc-7.8.2-x86_64-unknown-linux-deb7.tar.xz
 
 set -ev
-export PATH="~/.cabal/bin:$PATH"
-
-# Bootstrap cabal with sandbox via cabal-dev
-cabal update
-cabal install cabal-dev
-mkdir cabal-install
-pushd cabal-install
-cabal-dev install cabal-install-1.20.0.2
-sudo cp cabal-dev/bin/cabal /usr/local/bin/
-popd
 
 # Install HsColour
 mkdir hscolour
