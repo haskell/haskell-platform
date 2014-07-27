a32fbd017b61ea08129e9f40a2c620b6bcfb678f
Mark Lentczner <markl@glyphic.com>
2014-07-27 11:34:43 -0700

    more robust platform.sh
    handles host cabal versions that are pre-sandbox support
    checks for installed hptool version vs. build hptool version

--- platform.sh
+++ platform.sh
@@ -3,9 +3,9 @@
 set -e
 
 HPTOOL=hptool/dist/build/hptool/hptool
+HPTOOL_ALT=hptool/.cabal-sandbox/bin/hptool
 
-if [ \! \( -e $HPTOOL -a -x $HPTOOL \) ]
-then
+if ( cabal sandbox --help >/dev/null 2>&1 ) ; then
     if [ \! -d hptool/.cabal-sandbox ]
     then
         echo '***'
@@ -14,14 +14,25 @@ then
         cabal update
         (cd hptool; cabal sandbox init; cabal install --only-dependencies)
     fi
+else
+    if ( cabal install --dry-run --only-dependencies | grep -q 'would be installed' ) ; then
+        echo '=== pre-requisite packages for hptool are not installed'
+        echo '    run the following:'
+        echo '    cd hptool ; cabal install --only-dependencies'
+        exit 1
+    fi
+fi
 
-    echo '***'
-    echo '*** Building hptool'
-    echo '***'
-    (cd hptool; cabal build)
+echo '***'
+echo '*** Building hptool'
+echo '***'
+(cd hptool; cabal build)
+
+if [ "$HPTOOL_ALT" -nt "$HPTOOL" ] ; then
+    HPTOOL="$HPTOOL_ALT"
 fi
 
 echo '***'
 echo '*** Running hptool'
 echo '***'
-$HPTOOL "$@"
+exec $HPTOOL "$@"
