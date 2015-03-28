fa90210b908e26616904d8f0d7e2737ad2c29ad8
Mark Lentczner <markl@glyphic.com>
2015-03-28 15:29:25 -0700

    alpha 2

--- hptool/src/Releases2015.hs
+++ hptool/src/Releases2015.hs
@@ -9,14 +9,14 @@ releases2015 = [hp2015_2_0_0]
 hp2015_2_0_0 :: Release
 hp2015_2_0_0 =
     release "2015.2.0.0"
-        [ incGHC                            "7.10.0.20150316"
+        [ incGHC                            "7.10.1"
 
-        , incGHCLib "Cabal"                 "1.22.1.2"
+        , incGHCLib "Cabal"                 "1.22.2.0"
         , incGHCLib "array"                 "0.5.1.0"
         , incGHCLib "base"                  "4.8.0.0"
         , incGHCLib "bytestring"            "0.10.6.0"
         , incGHCLib "containers"            "0.5.6.2"
-        , incGHCLib "deepseq"               "1.4.1.0"
+        , incGHCLib "deepseq"               "1.4.1.1"
         , incGHCLib "directory"             "1.2.2.0"
         , incGHCLib "filepath"              "1.4.0.0"
         , incGHCLib "hpc"                   "0.6.0.2"
@@ -44,11 +44,11 @@ hp2015_2_0_0 =
         --, onlyWindows $ incGHCLib "Win32"   "2.3.1.0"
 
         , incLib "async"                    "2.0.2"
-        , incLib "attoparsec"               "0.12.1.3"  -- 0.10.4.0"
+        , incLib "attoparsec"               "0.12.1.4"
         , incLib "case-insensitive"         "1.2.0.4"
-        , incLib "cgi"                      "3001.2.2.0"
+        , incLib "cgi"                      "3001.2.2.1"
         , incLib "fgl"                      "5.5.1.0"
-        , incLib "GLUT"                     "2.6.0.1"
+        , incLib "GLUT"                     "2.7.0.0"
         , incLib "GLURaw"                   "1.5.0.0"
         , incLib "haskell-src"              "1.0.2.0"
         , incLib "hashable"                 "1.2.3.2"
@@ -57,11 +57,11 @@ hp2015_2_0_0 =
         , incLib "HUnit"                    "1.2.5.2"
         , incLib "mtl"                      "2.2.1"
         , incLib "network"                  "2.6.0.2"
-        , incLib "OpenGL"                   "2.11.1.0"
-        , incLib "OpenGLRaw"                "2.4.0.0"
+        , incLib "OpenGL"                   "2.12.0.0"
+        , incLib "OpenGLRaw"                "2.4.1.0"
         , incLib "parallel"                 "3.2.0.6"
         , incLib "parsec"                   "3.1.9"
-        , incLib "primitive"                "0.5.4.0"
+        , incLib "primitive"                "0.6"
         , incLib "QuickCheck"               "2.8"
         , incLib "random"                   "1.1"
         , incLib "regex-base"               "0.93.2"
@@ -72,7 +72,7 @@ hp2015_2_0_0 =
         , incLib "syb"                      "0.4.4"
         , incLib "text"                     "1.2.0.4"
         , incLib "unordered-containers"     "0.2.5.1"
-        , incLib "vector"                   "0.10.12.2"
+        , incLib "vector"                   "0.10.12.3"
         , incLib "zlib"                     "0.5.4.2"
 
         -- Libs required by newer version of stuff - but not cleared for HP
@@ -85,18 +85,22 @@ hp2015_2_0_0 =
             -- needed by cabal-install, cgi, & HTTP
 
         -- was split out of network, so was in HP, just under different pacakge
-        -- needed by cabal-install
         , incLib "network-uri"              "2.6.0.1"
             -- needed by cabal-install, cgi, & HTTP
 
         -- needed by cgi
-        , incLib "exceptions"               "0.6.1"
+        , incLib "exceptions"               "0.8.0.2"
+        , incLib "transformers-compat"      "0.4.0.4"
         , incLib "multipart"                "0.1.2"
 
         -- needed by attoparsec
         , incLib "scientific"               "0.3.3.8"
 
-        , incTool "cabal-install"           "1.22.0.1"
+        -- needed by OpenGL
+        , incLib "ObjectName"               "1.1.0.0"
+        , incLib "StateVar"                 "1.1.0.0"
+
+        , incTool "cabal-install"           "1.22.2.0"
         , incTool "alex"                    "3.1.4"
         , incTool "happy"                   "1.19.5"
 
