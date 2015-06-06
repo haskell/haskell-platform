6950d8cf21502090a308a48f04d8d0b4d560a3a6
Mark Lentczner <markl@glyphic.com>
2015-06-06 14:31:15 -0700

    change travis-ci build to use hvr's GHC builds

--- travis-setup.sh
+++ travis-setup.sh
@@ -5,10 +5,11 @@ GHC_TARBALL="$1"
 set -ev
 
 # Install HsColour
+cabal update
 mkdir hscolour
 pushd hscolour
 cabal sandbox init
-cabal install hscolour-1.20.3
+cabal install hscolour
 sudo cp .cabal-sandbox/bin/HsColour /usr/local/bin/
 popd
 
